7d9423249fe8e20dc030edfeefa9750d2886decc
fjmorais <fabiano.morais@outlook.com>
2020-12-17 14:01:58 -0300

    Atualizacao Script Manage Pack SCOM

--- SCOMQueries/QuandoUmAgentFoiInstaladoEQuandoUmManagementPackFoiInstalado.sql
+++ SCOMQueries/QuandoUmAgentFoiInstaladoEQuandoUmManagementPackFoiInstalado.sql
@@ -1,15 +1,15 @@
 Olá Fabiano, seguem as queries para consulta conforme solicitado:
 
-Select para saber quando o agente foi instalado:
+--Select para saber quando o agente foi instalado:
 
-USE OperaionsManager
+USE OperationsManager
 GO
 select [Path], FullName, TimeAdded from BaseManagedEntity
 where FullName like 'Microsoft.SystemCenter.HealthService:%'
 
-Select para saber quando o Management Pack foi instalado:
+--Select para saber quando o Management Pack foi instalado:
 
-USE OperaionsManager
+USE OperationsManager
 GO
 select MPFriendlyName, MPCreated from ManagementPack
 where MPFriendlyName like '%active directory%'
