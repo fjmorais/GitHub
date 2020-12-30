6f789912c1622d8501f0e06faa7898f76314d6c4
fjmorais <fabiano.morais@outlook.com>
2020-12-30 10:23:48 -0300

    today

--- dba_tool_Kit/ReplicationTables.sql
+++ dba_tool_Kit/ReplicationTables.sql
@@ -6,3 +6,10 @@ and name in ('
 corp_sub_cobertura',
 'corp_produto',
 'corp_forma_pagamento')
+
+
+SELECT s.name AS Schema_Name, tb.name AS Table_Name
+, tb.object_id, tb.type, tb.type_desc, is_replicated
+FROM sys.tables tb
+inner join sys.schemas s on s.schema_id = tb.schema_id
+WHERE is_replicated = 1
